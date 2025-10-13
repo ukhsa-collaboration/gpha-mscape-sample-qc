561de8e954065a728f2cee7a9aa6e51082129f5f
Deborah Greer <deborah.greer@ukhsa.gov.uk>
2025-10-13 11:44:04 +0000

    chore: Update all files with pre-commit run on all files checks

--- src/mscape_sample_qc/qc_functions.py
+++ src/mscape_sample_qc/qc_functions.py
@@ -3,24 +3,20 @@
 "Module containing functions required to QC a sample on mscape."
 
 # Imports
-import datetime
 import json
-import logging
 import os
-import time
-from importlib.metadata import version
-from onyx_analysis_helper import onyx_analysis_helper_functions as oa
 
 import pandas as pd
 import yaml
 from onyx import OnyxClient, OnyxConfig, OnyxEnv
-from onyx.exceptions import OnyxClientError, OnyxConfigError, OnyxConnectionError, OnyxHTTPError
+from onyx_analysis_helper import onyx_analysis_helper_functions as oa
 
 # Set up onyx config
 CONFIG = OnyxConfig(
     domain=os.environ[OnyxEnv.DOMAIN],
     token=os.environ[OnyxEnv.TOKEN],
 )
+
 
 # Functions
 @oa.call_to_onyx
@@ -31,15 +27,14 @@ def retrieve_sample_information(record_id: str, server: str) -> [pd.DataFrame, d
     """
     # Retrieve record info
     with OnyxClient(CONFIG) as client:
-        metadata_dict = client.get(
-            project = server,
-            climb_id = record_id)
+        metadata_dict = client.get(project=server, climb_id=record_id)
 
     # Pop the classifier information from the record dictionary object and convert to df:
-    classifier_df = pd.DataFrame(metadata_dict.pop('classifier_calls'))
+    classifier_df = pd.DataFrame(metadata_dict.pop("classifier_calls"))
     exitcode = 0
 
     return classifier_df, exitcode
+
 
 def read_config_file(config_file: os.path) -> dict:
     """Reads config file to get QC criteria to filter sequences against.
@@ -54,6 +49,7 @@ def read_config_file(config_file: os.path) -> dict:
 
     return qc_criteria
 
+
 def get_read_proportions(class_calls: pd.DataFrame) -> dict:
     """Gets proportions of reads that are in different subsets of the
     data including % unclassified, % host, % spike-in.
@@ -61,48 +57,57 @@ def get_read_proportions(class_calls: pd.DataFrame) -> dict:
         class_calls -- dataframe containing classifier information for
         reads in the sample
     Returns:
-        A dictionary of read proportion statistics for the relevant 
+        A dictionary of read proportion statistics for the relevant
         metrics.
     """
     taxa_dict = {}
-    taxa_dict['total_reads'] =  class_calls['count_direct'].sum().item()
+    taxa_dict["total_reads"] = class_calls["count_direct"].sum().item()
 
     try:
-        taxa_dict['count_descendants_unclassified'] = class_calls.loc[
-            class_calls['taxon_id'] == 0, 'count_descendants'].item()
-        taxa_dict['percentage_unclassified'] = class_calls.loc[
-            class_calls['taxon_id'] == 0, 'percentage'].item()
+        taxa_dict["count_descendants_unclassified"] = class_calls.loc[
+            class_calls["taxon_id"] == 0, "count_descendants"
+        ].item()
+        taxa_dict["percentage_unclassified"] = class_calls.loc[
+            class_calls["taxon_id"] == 0, "percentage"
+        ].item()
     except ValueError:
-        taxa_dict['count_descendants_unclassified'] = 0
-        taxa_dict['percentage_unclassified'] = 0
+        taxa_dict["count_descendants_unclassified"] = 0
+        taxa_dict["percentage_unclassified"] = 0
 
     try:
-        taxa_dict['count_descendants_spike_in'] = class_calls.loc[
-            class_calls['is_spike_in'] == True, 'count_descendants'].item()
-        taxa_dict['percentage_spike_in'] = class_calls.loc[
-            class_calls['is_spike_in'] == True, 'percentage'].item()
+        taxa_dict["count_descendants_spike_in"] = class_calls.loc[
+            class_calls["is_spike_in"] == True, "count_descendants"
+        ].item()
+        taxa_dict["percentage_spike_in"] = class_calls.loc[
+            class_calls["is_spike_in"] == True, "percentage"
+        ].item()
     except ValueError:
-        taxa_dict['count_descendants_spike_in'] = 0
-        taxa_dict['percentage_spike_in'] = 0
+        taxa_dict["count_descendants_spike_in"] = 0
+        taxa_dict["percentage_spike_in"] = 0
 
     try:
-        taxa_dict['count_descendants_host'] = class_calls.loc[
-            class_calls['taxon_id'] == 9606, 'count_descendants'].item()
-        taxa_dict['percentage_host'] = class_calls.loc[
-            class_calls['taxon_id'] == 9606, 'percentage'].item()
+        taxa_dict["count_descendants_host"] = class_calls.loc[
+            class_calls["taxon_id"] == 9606, "count_descendants"
+        ].item()
+        taxa_dict["percentage_host"] = class_calls.loc[
+            class_calls["taxon_id"] == 9606, "percentage"
+        ].item()
     except ValueError:
-        taxa_dict['count_descendants_host'] = 0
-        taxa_dict['percentage_host'] = 0
+        taxa_dict["count_descendants_host"] = 0
+        taxa_dict["percentage_host"] = 0
 
     try:
-        taxa_dict['count_descendants_genus'] = class_calls.loc[
-            class_calls['rank'] == "G", 'count_descendants'].sum().item()
-        taxa_dict['percentage_genus'] = (taxa_dict['count_descendants_genus']
-                                          / taxa_dict['total_reads']) * 100
+        taxa_dict["count_descendants_genus"] = (
+            class_calls.loc[class_calls["rank"] == "G", "count_descendants"].sum().item()
+        )
+        taxa_dict["percentage_genus"] = (
+            taxa_dict["count_descendants_genus"] / taxa_dict["total_reads"]
+        ) * 100
     except ValueError:
-        taxa_dict['count_descendants_genus'] = 0
+        taxa_dict["count_descendants_genus"] = 0
 
     return taxa_dict
+
 
 def check_thresholds(result_dict: dict, threshold_dict: dict) -> dict:
     """For each threshold, check if value falls with pass/warn/fail
@@ -117,23 +122,24 @@ def check_thresholds(result_dict: dict, threshold_dict: dict) -> dict:
     for metric in threshold_dict:
         dict_key = f"{metric}_qc"
         # Large values = better, lower values = fail
-        if threshold_dict[metric]['pass'] > threshold_dict[metric]['fail']:
-            if result_dict[metric] >= threshold_dict[metric]['pass']:
+        if threshold_dict[metric]["pass"] > threshold_dict[metric]["fail"]:
+            if result_dict[metric] >= threshold_dict[metric]["pass"]:
                 result_dict[dict_key] = "Pass"
-            elif result_dict[metric] > threshold_dict[metric]['fail']:
+            elif result_dict[metric] > threshold_dict[metric]["fail"]:
                 result_dict[dict_key] = "Warn"
-            elif result_dict[metric] <= threshold_dict[metric]['fail']:
+            elif result_dict[metric] <= threshold_dict[metric]["fail"]:
                 result_dict[dict_key] = "Fail"
         # Large values = fail, lower values = pass
         else:
-            if result_dict[metric] <= threshold_dict[metric]['pass']:
+            if result_dict[metric] <= threshold_dict[metric]["pass"]:
                 result_dict[dict_key] = "Pass"
-            elif result_dict[metric] < threshold_dict[metric]['fail']:
+            elif result_dict[metric] < threshold_dict[metric]["fail"]:
                 result_dict[dict_key] = "Warn"
-            elif result_dict[metric] >= threshold_dict[metric]['fail']:
+            elif result_dict[metric] >= threshold_dict[metric]["fail"]:
                 result_dict[dict_key] = "Fail"
 
     return result_dict
+
 
 def check_spike_detected(qc_results_dict: dict) -> dict:
     """Check if spike in detected and add results to qc results dict
@@ -143,13 +149,14 @@ def check_spike_detected(qc_results_dict: dict) -> dict:
         qc_results_dict -- dictionary containing QC result values that has
         been updated with spike in detection result.
     """
-    if qc_results_dict['count_descendants_spike_in'] == 0:
-        qc_results_dict['spike_detected'] = "Fail"
-        qc_results_dict['percentage_spike_in_qc'] = "NA"
+    if qc_results_dict["count_descendants_spike_in"] == 0:
+        qc_results_dict["spike_detected"] = "Fail"
+        qc_results_dict["percentage_spike_in_qc"] = "NA"
     else:
-        qc_results_dict['spike_detected'] = "Pass"
+        qc_results_dict["spike_detected"] = "Pass"
 
     return qc_results_dict
+
 
 def get_headline_result(qc_results) -> str:
     "Checks QC results and assigns a top level result"
@@ -159,6 +166,7 @@ def get_headline_result(qc_results) -> str:
         headline_result = "QC results passed thresholds"
 
     return headline_result
+
 
 def write_qc_results_to_json(qc_dict: dict, sample_id: str, results_dir: os.path) -> os.path:
     """Write qc results dictionary to json output file.
@@ -171,14 +179,20 @@ def write_qc_results_to_json(qc_dict: dict, sample_id: str, results_dir: os.path
     """
     result_file = os.path.join(results_dir, f"{sample_id}_qc_results.json")
 
-    with open(result_file, "w", encoding = "utf-8") as file:
+    with open(result_file, "w", encoding="utf-8") as file:
         json.dump(qc_dict, file)
 
     return result_file
 
-def create_analysis_fields(record_id: str, qc_thresholds: dict,
-                           qc_results: dict, server: str,
-                           headline_result: str, result_file: os.path) -> dict:
+
+def create_analysis_fields(
+    record_id: str,
+    qc_thresholds: dict,
+    qc_results: dict,
+    server: str,
+    headline_result: str,
+    result_file: os.path,
+) -> dict:
     """Set up fields dictionary used to populate analysis table containing
     QC metrics.
     Arguments:
@@ -194,12 +208,12 @@ def create_analysis_fields(record_id: str, qc_thresholds: dict,
     onyx_analysis = oa.OnyxAnalysis()
     onyx_analysis.add_analysis_details(
         analysis_name="ukhsa-classifier-qc-metrics",
-        analysis_description="""This is an analysis to generate QC statistics for individual samples"""
+        analysis_description="This is an analysis to generate QC statistics for individual samples",
     )
-    onyx_analysis.add_package_metadata(package_name = "mscape-sample-qc")
-    methods_fail = onyx_analysis.add_methods(methods_dict = qc_thresholds)
-    results_fail = onyx_analysis.add_results(top_result = headline_result, results_dict = qc_results)
-    onyx_analysis.add_server_records(sample_id = record_id, server_name = "synthscape")
+    onyx_analysis.add_package_metadata(package_name="mscape-sample-qc")
+    methods_fail = onyx_analysis.add_methods(methods_dict=qc_thresholds)
+    results_fail = onyx_analysis.add_results(top_result=headline_result, results_dict=qc_results)
+    onyx_analysis.add_server_records(sample_id=record_id, server_name="synthscape")
     output_fail = onyx_analysis.add_output_location(result_file)
     required_field_fail, attribute_fail = onyx_analysis.check_analysis_object()
 
--- src/mscape_sample_qc/qc_sample.py
+++ src/mscape_sample_qc/qc_sample.py
@@ -17,30 +17,54 @@ import mscape_sample_qc.qc_functions as qc
 def get_args():
     """Get command line arguments"""
     parser = argparse.ArgumentParser(
-        prog = "QC sample", description = """Wrapper used to quality
+        prog="QC sample",
+        description="""Wrapper used to quality
         check mscape samples against a set of pre-defined criteria.
-        Results are returned in a json format. 
-        """
+        Results are returned in a json format.
+        """,
     )
-    parser.add_argument("--input", "-i", type=str, required=True,
-                        help="Sample ID")
-    parser.add_argument("--config", "-c", type=str, required=False,
-                        help="Path to file with QC criteria")
-    parser.add_argument("--output", "-o", type=str, required=True,
-                        help="Folder to save QC results to")
-    parser.add_argument("--server", "-s", type=str, required=True,
-                        choices=["mscape", "synthscape"],
-                        help="Specify server code is being run on")
-    parser.add_argument("--no-onyx", required=False, action="store_true",
-                        help="Use this option to only write results to file")
-    parser.add_argument("--store-onyx", required=False, action="store_true",
-                        help="Use this option to store results as an onyx analysis object for later uplaod")
-    parser.add_argument("--test-onyx", required=False, action="store_true",
-                        help="Use this option to do a test upload and check for errors before attempting an upload to onyx")
-    parser.add_argument("--prod-onyx", required=False, action="store_true",
-                        help="Use this option to upload results to onyx")
+    parser.add_argument("--input", "-i", type=str, required=True, help="Sample ID")
+    parser.add_argument(
+        "--config", "-c", type=str, required=False, help="Path to file with QC criteria"
+    )
+    parser.add_argument(
+        "--output", "-o", type=str, required=True, help="Folder to save QC results to"
+    )
+    parser.add_argument(
+        "--server",
+        "-s",
+        type=str,
+        required=True,
+        choices=["mscape", "synthscape"],
+        help="Specify server code is being run on",
+    )
+    parser.add_argument(
+        "--no-onyx",
+        required=False,
+        action="store_true",
+        help="Use this option to only write results to file",
+    )
+    parser.add_argument(
+        "--store-onyx",
+        required=False,
+        action="store_true",
+        help="Use this option to store results as an onyx analysis object for later uplaod",
+    )
+    parser.add_argument(
+        "--test-onyx",
+        required=False,
+        action="store_true",
+        help="Use this option to do a test upload and check for errors before attempting an upload to onyx",
+    )
+    parser.add_argument(
+        "--prod-onyx",
+        required=False,
+        action="store_true",
+        help="Use this option to upload results to onyx",
+    )
 
     return parser.parse_args()
+
 
 def set_up_logger(stdout_file):
     """Creates logger for component - all logging messages go to stdout
@@ -71,8 +95,9 @@ def main():
         lib_path = resources.files("mscape_sample_qc") / "lib"
         with resources.as_file(lib_path / "qc_thresholds.yaml") as config_path:
             args.config = str(config_path)
-        logging.info("No config file specified, using default parameters from file: %s",
-                     args.config)
+        logging.info(
+            "No config file specified, using default parameters from file: %s", args.config
+        )
 
     else:
         logging.info("Reading qc parameters from file provided: %s", args.config)
@@ -97,7 +122,7 @@ def main():
     qc_results_dict = qc.get_read_proportions(class_df)
 
     # Get qc status for relevant sample level metrics
-    qc_results_dict = qc.check_thresholds(qc_results_dict, threshold_dict['sample_thresholds'])
+    qc_results_dict = qc.check_thresholds(qc_results_dict, threshold_dict["sample_thresholds"])
 
     # Check spike detected
     qc_results_dict = qc.check_spike_detected(qc_results_dict)
@@ -112,32 +137,35 @@ def main():
     ## Add QC metrics to onyx
     # Set up data for entry in analysis table
     headline_result = qc.get_headline_result(qc_results_dict)
-    onyx_analysis, exitcode = qc.create_analysis_fields(args.input,
-                                                  threshold_dict['sample_thresholds'],
-                                                  qc_results_dict,
-                                                  args.server,
-                                                  headline_result,
-                                                  qc_result_file)
+    onyx_analysis, exitcode = qc.create_analysis_fields(
+        args.input,
+        threshold_dict["sample_thresholds"],
+        qc_results_dict,
+        args.server,
+        headline_result,
+        qc_result_file,
+    )
 
     if exitcode == 1:
         logging.error("Invalid attribute in analysis fields submitted, check logs for details")
         return exitcode
 
-    #Add data to analysis table
+    # Add data to analysis table
     if args.store_onyx:
         onyx_json_file = os.path.join(args.output, f"{args.input}_qc_metrics_analysis_fields.json")
-        result_file = onyx_analysis.write_analysis_to_json(result_file = onyx_json_file)
+        result_file = onyx_analysis.write_analysis_to_json(result_file=onyx_json_file)
         logging.info("Onyx analysis fields written to file %s", result_file)
         exitcode = 0
         return result_file, exitcode
 
     if args.test_onyx:
-        result, exitcode = onyx_analysis.write_analysis_to_onyx(server = args.server, dryrun = True)
+        result, exitcode = onyx_analysis.write_analysis_to_onyx(server=args.server, dryrun=True)
 
     if args.prod_onyx:
-        result, exitcode = onyx_analysis.write_analysis_to_onyx(server = args.server, dryrun = False)
+        result, exitcode = onyx_analysis.write_analysis_to_onyx(server=args.server, dryrun=False)
 
     return result, exitcode
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     sys.exit(main())
